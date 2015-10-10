eb51968f79a30a58cd341ab31ee9aa3d5c0a4895
mayet <mayet@mail.ru>
2015-10-10 20:51:27 +0300

    xzc

---
+++ Anagram/Anagram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Anagram
+{
+    class Anagram
+    {
+        public Anagram()
+        {
+            StreamReader Reader = new StreamReader(@"C:\\c#\newFile.txt", Encoding.UTF8);
+            File.Create(@"C:\\c#\Result.txt");
+            //File.Create(@"C:\\c#\tmpRegist.txt");
+            StreamReader Regist = new StreamReader(@"C:\\c#\tmpRegist.txt", Encoding.UTF8);
+            StreamWriter RegistW = new StreamWriter(@"C:\\c#\tmpRegist.txt", Encoding.UTF8);
+
+
+            string word = null;
+            string tmpWord = null;
+
+
+            IsAnagram flag = new IsAnagram();
+            while ((word = Reader.ReadLine()) != null)
+            {
+                if((tmpWord = Regist.ReadLine()) != null)
+                    while((tmpWord = Regist.ReadLine()) != null
+                    {
+                        if(flag.check(word, tmpWord) == 1)
+                        Console.WriteLine(word);
+                    }
+               
+            }
+
+            //FileStream lenth = new FileStream.Lenght;
+            Console.WriteLine();
+            Reader.Close();
+            Regist.Close();
+        }
+    }
+}
---
+++ Anagram/IsAnagram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class IsAnagram
+    {
+        public int check (string word, string tmpWord)
+        {
+            if((word.Length != tmpWord.Length) || (comparison(word, tmpWord) == 0) )
+                return 0;
+            return 1;
+        }
+
+        static int comparison (string word, string tmpWord)
+        {
+            int n=0, k=0;
+            for (int i = 0; i != word.Length; i++)
+                for (int j = 0; j != tmpWord.Length; j++)
+                    if (word[i] == tmpWord[j])
+                        n++;
+            for (int i = 0; i != word.Length; i++)
+                for (int j = 0; j != word.Length; j++)
+                    if (word[i] == word[j])
+                        k++;
+
+            if (n == k)
+                return 1;
+            return 0;
+        }
+    }
+}
