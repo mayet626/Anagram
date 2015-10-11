9367cad10abc8ef1093eb5ffc5fe047c025014d5
mayet <mayet@mail.ru>
2015-10-11 19:19:48 +0300

    w

---
+++ Anagram/AddToTmp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Anagram
+{
+    class AddToTmp
+    {
+        public AddToTmp(string word)
+        {
+            StreamWriter writer = new StreamWriter(@"C:\\c#\tmpRegist.txt", true, Encoding.UTF8);
+            writer.WriteLine(word);
+            writer.Close();
+        }
+    }
+}
